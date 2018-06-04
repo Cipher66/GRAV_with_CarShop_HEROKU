e7ee5cf76f51ac4ceb03e40fe740fc95a9fb92ee
Cipher66 <sergiochavezms22@outlook.com>
2018-06-04 19:19:13 +0200

    arreglos

--- user/themes/car-shop/cache/compiled/files/5febde976892bc2fecba0f0a2825c15f.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/var/www/html/grav/user/plugins/devtools/devtools.yaml',
-    'modified' => 1523556171,
-    'data' => [
-        'enabled' => true
-    ]
-];
