14232290bf1aa5495bf0ec4f4108a892d100851b
Cipher66 <sergiochavezms22@outlook.com>
2018-06-04 09:24:22 +0200

    cosas

---
+++ user/themes/car-shop/cache/compiled/files/c5b16cac40f1cb99b3432d75db709d8b.yaml.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/var/www/html/grav/user/config/plugins/user-information-plugin.yaml',
+    'modified' => 1524325384,
+    'data' => [
+        'enabled' => false,
+        'text_var' => 'Custom Text added by the **User Information Plugin** plugin (disable plugin to remove)'
+    ]
+];
